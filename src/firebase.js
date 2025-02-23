98368044134b8bd01d7bdafa49ccbcdba5a4f943
MAD_DRAX <k.d.n8125u@gmail.com>
2025-02-24 00:13:22 +0530

    update Firebase configuration with hardcoded values

--- src/firebase.js
+++ src/firebase.js
@@ -11,12 +11,12 @@ import { toast } from "react-toastify";
 
 // Firebase configuration using environment variables
 const firebaseConfig = {
-    apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
-    authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
-    projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
-    storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
-    messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
-    appId: process.env.REACT_APP_FIREBASE_APP_ID,
+    apiKey: "AIzaSyClQKgVHy9oiaA0L2UIrju6JKWdhPZoVyk",
+    authDomain: "netflix-clone-df734.firebaseapp.com",
+    projectId: "netflix-clone-df734",
+    storageBucket: "netflix-clone-df734.firebasestorage.app",
+    messagingSenderId: "820218212525",
+    appId: "1:820218212525:web:a68d360a6a5883b992d624",
 };
 
 // Initialize Firebase
