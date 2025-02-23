fca77faa4e6305e8797e7d1f6c00a4341eac20e1
MAD_DRAX <k.d.n8125u@gmail.com>
2025-02-24 00:01:13 +0530

    add dotenv for environment variable management and update Firebase configuration

--- src/firebase.js
+++ src/firebase.js
@@ -8,17 +8,15 @@ import {
 } from "firebase/auth";
 import { addDoc, collection, getFirestore } from "firebase/firestore";
 import { toast } from "react-toastify";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
 
-// Your web app's Firebase configuration
+// Firebase configuration using environment variables
 const firebaseConfig = {
-    apiKey: "Your API key",
-    authDomain: "Your Auth Domain",
-    projectId: "Your Project ID",
-    storageBucket: "Your storageBucket key",
-    messagingSenderId: "Your messagingSenderId key",
-    appId: "Your appId",
+    apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+    authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+    projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+    storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+    messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+    appId: process.env.REACT_APP_FIREBASE_APP_ID,
 };
 
 // Initialize Firebase
@@ -38,7 +36,6 @@ const signup = async (name, email, password) => {
         });
     } catch (error) {
         console.log(error);
-        // alert(error.message);
         toast.error(error.code.split("/")[1].split("-").join(" "));
     }
 };
@@ -48,7 +45,6 @@ const login = async (email, password) => {
         await signInWithEmailAndPassword(auth, email, password);
     } catch (error) {
         console.log(error);
-        // alert(error.message);
         toast.error(error.code.split("/")[1].split("-").join(" "));
     }
 };
